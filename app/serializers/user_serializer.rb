601dcf2542a01965a43320c763c5cf52bad175c9
ShobhitRatan <shobhit.ratan@baruchmail.cuny.edu>
2020-09-04 17:29:10 -0400

    commit

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :image_1, :image_2, :addresses, :work_experiences, :educations, :awards, 
+  attributes :id, :email, :name, :image_1, :image_2, :addresses, :work_experiences, :educations, :awards 
   has_many :addresses 
   has_many :work_experiences 
   has_many :educations
   has_many :awards 
-  
-
 end
